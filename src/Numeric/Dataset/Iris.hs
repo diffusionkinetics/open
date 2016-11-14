c21a8382599e6592e7f34eaf2422fdbb5f749058
Tom Nielsen <tanielsen@gmail.com>
2016-11-14 22:31:25 +0000

    basic api; iris dataset

--- src/Numeric/Dataset/Iris.hs
+++ src/Numeric/Dataset/Iris.hs
@@ -1,1 +1,38 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+
+{-|
+
+The classical Iris dataset, due to R.A. Fisher.
+
+UCI ML Repository link <https://archive.ics.uci.edu/ml/datasets/Iris>
+
+-}
+
 module Numeric.Dataset.Iris where
+
+import Numeric.Dataset.UCI
+
+import Data.Csv
+import GHC.Generics
+
+data IrisClass = Setosa | Versicolor | Virginica
+  deriving (Show, Read, Eq, Generic)
+
+instance FromField IrisClass where
+  parseField "Iris-setosa" = pure Setosa
+  parseField "Iris-versicolor" = pure Versicolor
+  parseField "Iris-virginica" = pure Virginica
+  parseField _ = fail "unknown iris class"
+
+data Iris = Iris
+  { sepalLength :: Double
+  , sepalWidth :: Double
+  , petalLength :: Double
+  , petalWidth :: Double
+  , irisClass :: IrisClass
+  } deriving (Show, Read, Generic)
+
+instance FromRecord Iris
+
+iris :: Dataset Iris
+iris = csvDataset "http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
--- src/Numeric/Dataset/UCI.hs
+++ src/Numeric/Dataset/UCI.hs
@@ -1,1 +1,41 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Numeric.Dataset.UCI where
+
+import Network.HTTP
+import Data.Csv
+import System.FilePath
+import System.Directory
+import Data.Hashable
+import Data.Monoid
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+
+
+type Dataset a = FilePath -- ^ Directory for caching downloaded datasets
+                 -> IO [a]
+type URL = String
+
+-- |Retrieve a dataset
+getDataset :: Dataset a -> IO [a]
+getDataset ds = ds "/tmp/haskds"
+
+csvDataset :: FromRecord a => URL -> Dataset a
+csvDataset url cacheDir = do
+  createDirectoryIfMissing True cacheDir
+  let fnm = cacheDir </> "ds" <> show (hash url)
+      parseFile contents = do
+        case decode NoHeader contents of
+          Right theData -> return $ V.toList theData
+          Left err -> fail err
+      castRequest :: Request String -> Request BL.ByteString
+      castRequest r = Request (rqURI r) (rqMethod r) (rqHeaders r) ""
+
+  ex <- doesFileExist fnm
+  if ex
+     then BL.readFile fnm >>= parseFile
+     else do
+       rsp <- simpleHTTP (castRequest $ getRequest url)
+       bs <- getResponseBody rsp
+       BL.writeFile fnm bs
+       parseFile bs
