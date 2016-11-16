b3f59a7b2a523114db604e69be704810e74b8c38
Tom Nielsen <tomn@beautifuldestinations.com>
2016-11-16 22:38:41 +0000

    Adult data set (#1)
    * more adult fields
    
    * finish adult
    
    * drop lines
    
    * adult test set

--- src/Numeric/Dataset/UCI.hs
+++ src/Numeric/Dataset/UCI.hs
@@ -11,6 +11,10 @@ import Data.Monoid
 import qualified Data.ByteString.Lazy as BL
 import qualified Data.Vector as V
 
+import Data.Char (toUpper)
+import Text.Read (readMaybe)
+import Data.ByteString.Char8 (unpack)
+import qualified Data.ByteString.Lazy.Char8 as BL8
 
 type Dataset a = FilePath -- ^ Directory for caching downloaded datasets
                  -> IO [a]
@@ -20,12 +24,12 @@ type URL = String
 getDataset :: Dataset a -> IO [a]
 getDataset ds = ds "/tmp/haskds"
 
-csvDataset :: FromRecord a => URL -> Dataset a
-csvDataset url cacheDir = do
+csvDatasetDropLines :: FromRecord a => Int -> URL -> Dataset a
+csvDatasetDropLines dlns url cacheDir = do
   createDirectoryIfMissing True cacheDir
   let fnm = cacheDir </> "ds" <> show (hash url)
       parseFile contents = do
-        case decode NoHeader contents of
+        case decode NoHeader (dropLines dlns contents) of
           Right theData -> return $ V.toList theData
           Left err -> fail err
       castRequest :: Request String -> Request BL.ByteString
@@ -39,3 +43,21 @@ csvDataset url cacheDir = do
        bs <- getResponseBody rsp
        BL.writeFile fnm bs
        parseFile bs
+
+csvDataset :: FromRecord a =>  URL -> Dataset a
+csvDataset  = csvDatasetDropLines 0
+
+dashToCamelCase :: String -> String
+dashToCamelCase ('-':c:cs) = toUpper c : dashToCamelCase cs
+dashToCamelCase (c:cs) = c : dashToCamelCase cs
+dashToCamelCase [] = []
+
+parseDashToCamelField :: Read a => Field -> Parser a
+parseDashToCamelField s =
+  case readMaybe (dashToCamelCase $ unpack s) of
+    Just wc -> pure wc
+    Nothing -> fail "unknown"
+
+dropLines :: Int -> BL.ByteString -> BL.ByteString
+dropLines 0 s = s
+dropLines n s = dropLines (n-1) $ BL.tail $ BL8.dropWhile (/='\n') s
