fc84dea6f38a52a47e1baff5669acd6a248f24dd
Tom Nielsen <tanielsen@gmail.com>
2016-11-18 15:08:18 +0000

    first plot

---
+++ TestPlotly.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE OverloadedStrings,ExtendedDefaultRules #-}
+
+import Lucid
+import Lucid.Html5
+import Graphics.Plotly
+
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as T
+
+main = T.putStrLn $ renderText $ doctypehtml_ $ do
+  head_ $ do meta_ [charset_ "utf-8"]
+             script_ [src_ "https://cdn.plot.ly/plotly-latest.min.js"] ""
+--  head_ $ script_ "hello"
+  body_ $ do div_ [id_ "myDiv", style_ "width: 480px; height: 400px;"] ""
+             newPlot "myDiv" myTrace Nothing
+             p_ "hello world"
+
+myTrace = Trace [1,2,3,4] [5,3,7,2] [Markers] Scatter
--- src/Graphics/Plotly.hs
+++ src/Graphics/Plotly.hs
@@ -1,1 +1,51 @@
+{-# LANGUAGE DeriveGeneric, OverloadedStrings,FlexibleInstances #-}
+
 module Graphics.Plotly where
+
+import Data.Aeson
+import Data.Aeson.Types
+import Lucid
+import Data.Char (toLower)
+import Data.List (intercalate)
+import Data.Monoid ((<>))
+import Data.Text (pack)
+import Data.Text.Encoding (decodeUtf8)
+import Data.ByteString.Lazy (toStrict)
+import GHC.Generics
+
+data Mode = Markers | Lines deriving Show
+
+instance ToJSON [Mode] where
+  toJSON = toJSON . intercalate "+" . map (map toLower . show)
+
+data Type_ = Scatter deriving Show
+
+instance ToJSON Type_ where
+  toJSON = toJSON . map toLower . show
+
+
+data Trace = Trace
+  { x :: [Double]
+  , y :: [Double]
+  , mode :: [Mode]
+  , _type :: Type_
+  } deriving Generic
+
+instance ToJSON Trace where
+  toJSON = genericToJSON defaultOptions {omitNothingFields = True,
+                                         fieldLabelModifier = dropLeadingUnderscore}
+
+
+dropLeadingUnderscore :: String -> String
+dropLeadingUnderscore ('_':s) = s
+dropLeadingUnderscore s = s
+
+data Layout = Layout deriving Generic
+
+instance ToJSON Layout where
+  toJSON = genericToJSON defaultOptions {omitNothingFields = True}
+
+newPlot :: String -> Trace -> Maybe Layout -> Html ()
+newPlot divNm _tr _ =
+  let trJSON = decodeUtf8 $ toStrict $ encode _tr
+  in script_ ("Plotly.newPlot('"<>pack divNm<>"', ["<>trJSON<>"]);")
