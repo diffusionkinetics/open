58c397b28c70f34608ce66d2622c78fac5df34ee
Tom Nielsen <tanielsen@gmail.com>
2016-12-07 18:45:02 +0000

    gendoc template

--- gendoc/GenDoc.hs
+++ gendoc/GenDoc.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings,ExtendedDefaultRules #-}
 
+module Main where
+
 import Lucid
 import Lucid.Html5
 import Graphics.Plotly
@@ -9,14 +11,14 @@ import Lens.Micro
 import qualified Data.Text.Lazy as T
 import qualified Data.Text.Lazy.IO as T
 
-main = T.writeFile "plotlyTest.html" $ renderText $ doctypehtml_ $ do
+main = T.writeFile "../doc/index.html" $ renderText $ doctypehtml_ $ do
   head_ $ do meta_ [charset_ "utf-8"]
              plotlyCDN
---  head_ $ script_ "hello"
   body_ $ do p_ "hello world"
              toHtml $ plotly "myDiv" [myTrace] & layout . title ?~  "my plot"
                                                & layout . margin ?~  titleMargins
              p_ "hello world"
+
 
 myTrace = scatter & x ?~ [1,2,3,4]
                   & y ?~ [500,3000,700,200]
