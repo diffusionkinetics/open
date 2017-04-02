fe72867db5e157d9c548c88ab6caf84d11be405c
Tom Nielsen <tanielsen@gmail.com>
2017-04-02 20:42:50 +0100

    ask for plotlies

--- inliterate/lib/Inliterate/Class.hs
+++ inliterate/lib/Inliterate/Class.hs
@@ -1,8 +1,15 @@
-{-# LANGUAGE OverloadedStrings, DefaultSignatures #-}
+{-# LANGUAGE OverloadedStrings, DefaultSignatures, TypeSynonymInstances, FlexibleInstances #-}
 
 module Inliterate.Class where
 
 import Data.Time
+import Graphics.Plotly
+import Graphics.Plotly.Lucid
+import qualified Data.Text as T
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.IO as T
+import qualified Data.Text.Lazy.IO as TL
+import Lucid
 
 class AskInliterate a where
   askInliterate :: String -> a -> IO ()
@@ -20,3 +27,11 @@ instance AskInliterate Float
 instance AskInliterate UTCTime
 
 instance (Show a, Show b) => AskInliterate (a,b)
+
+instance AskInliterate (Html a) where
+  askInliterate _ html
+     = TL.putStrLn $ renderText html
+
+instance AskInliterate Plotly where
+  askInliterate _ plt
+     = TL.putStrLn $ renderText $ toHtml plt
