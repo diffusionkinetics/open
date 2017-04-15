959e2fc97a3b9cd5e27e8597ba5d5d7991edb111
Marco Zocca <marco.zocca@recordunion.com>
2017-04-15 16:24:11 +0200

    started Numeric.Datasets.Netflix

---
+++ datasets/src/Numeric/Datasets/Netflix.hs
@@ -0,0 +1,16 @@
+{-|
+Netflix prize dataset
+
+From the README :
+
+The movie rating files contain over 100 million ratings from 480 thousand randomly-chosen, anonymous Netflix customers over 17 thousand movie titles.  The data were collected between October, 1998 and December, 2005 and reflect the distribution of all ratings received during this period.  The ratings are on a scale from 1 to 5 (integral) stars. To protect customer privacy, each customer id has been replaced with a randomly-assigned id.  The date of each rating and the title and year of release for each movie id are also provided.
+
+The competition ended on September, 2009, and the dataset was subsequently removed from the public domain by the company. 
+
+For further information, see <http://netflixprize.com/>
+-}
+
+module Numeric.Datasets.Netflix where
+
+import Numeric.Datasets
+import Data.Csv
