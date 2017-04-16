fd38c7859d847307b198bab1f85e38b973cb9fe5
ocramz <zocca.marco@gmail.com>
2017-04-16 15:19:21 +0200

    NB: ReadAs not implemented

--- datasets/src/Numeric/Datasets/Netflix.hs
+++ datasets/src/Numeric/Datasets/Netflix.hs
@@ -8,9 +8,9 @@ The movie rating files contain over 100 million ratings from 480 thousand random
 
 The competition ended on September, 2009, and the dataset was subsequently removed from the public domain by the company. 
 
-We include in this repository a small mock dataset in the same format for development purposes.
+We include in this repository a tiny subset of the original dataset for development purposes.
 
-For further information, see <http://netflixprize.com/>
+For further information, see <http://netflixprize.com/>.
 -}
 
 module Numeric.Datasets.Netflix where
@@ -29,6 +29,7 @@ import Data.Attoparsec.ByteString.Char8 hiding (takeWhile, inClass)
 
 
 
+-- | Dataset files. The directories are scanned recursively and their contents are presented as (FilePath, ByteString) pairs
 
 trainingSet :: [(FilePath, ByteString)]
 trainingSet = $(embedDir "datafiles/netflix/training/")
@@ -36,23 +37,128 @@ trainingSet = $(embedDir "datafiles/netflix/training/")
 testSet :: [(FilePath, ByteString)]
 testSet = $(embedDir "datafiles/netflix/test/")
 
+movies :: [(FilePath, ByteString)]
+movies = $(embedDir "datafiles/netflix/movies/")
 
 
--- Attoparsec
+
+-- * Data types
+
+data Rating = Rating {userId :: UserId,
+                      ratingDate :: Day} deriving (Eq, Show)
+
+-- training set
+newtype UserId = UserId {unUserId :: Int} deriving Eq
+instance Show UserId where show = show . unUserId
+
+data Train = Train {trainRating :: Rating,
+                    rating :: Int } deriving (Eq, Show)
+
+-- movies file
+newtype MovieId = MovieId {unMovieId :: Int} deriving Eq
+instance Show MovieId where show = show . unMovieId
+
+data Movie = Movie { movieId :: MovieId,
+                     releaseYear :: Day,
+                     movieTitle :: ByteString } deriving (Eq, Show)
+
+-- "qualifying" file (test set)
+newtype Test = Test { testRating :: Rating } deriving (Eq, Show)
+
+
+
+
+
+-- * Netflix dataset parsers
+
+{-The first line of each training set file contains the movie id followed by a
+colon.  Each subsequent line in the file corresponds to a rating from a customer
+and its date in the following format:
+
+CustomerID,Rating,Date
+
+- MovieIDs range from 1 to 17770 sequentially.
+- CustomerIDs range from 1 to 2649429, with gaps. There are 480189 users.
+- Ratings are on a five star (integral) scale from 1 to 5.
+- Dates have the format YYYY-MM-DD.-}
+trainingSetParser :: PT.Parser ByteString (MovieId, [Train])
+trainingSetParser = stanza trainRow
+
+{-The test set ("qualifying") file consists of lines indicating a movie id, followed by a colon, and then customer ids and rating dates, one per line for that movie id.
+The movie and customer ids are contained in the training set.  Of course the
+ratings are withheld. There are no empty lines in the file.-}
+testSetParser :: PT.Parser ByteString [(MovieId, [Test])]
+testSetParser = many1 (stanza testRow)
+
+{-Movie information is in the following format:
+
+MovieID,YearOfRelease,Title
+
+- MovieID do not correspond to actual Netflix movie ids or IMDB movie ids.
+- YearOfRelease can range from 1890 to 2005 and may correspond to the release of
+  corresponding DVD, not necessarily its theaterical release.
+- Title is the Netflix movie title and may not correspond to 
+  titles used on other sites.  Titles are in English.-}
+moviesParser :: PT.Parser ByteString [Movie]
+moviesParser = parseRows moviesRow
+
+
+
+
+-- * Netflix dataset row type parsers
+
+trainRow :: PT.Parser ByteString Train
+trainRow = do
+  uid <- decc
+  rate <- decc
+  d <- date
+  let r = Rating (UserId uid) d
+  return $ Train r rate
+  
+testRow :: PT.Parser ByteString Test
+testRow = do
+  uid <- decc
+  d <- date
+  let r = Rating (UserId uid) d
+  return $ Test r
+
+moviesRow :: PT.Parser ByteString Movie
+moviesRow = do
+  mo <- decc
+  ye <- decc
+  title <- takeWhile (inClass "-a-zA-Z0-9 :,&.")
+  return $ Movie (MovieId mo) (fromGregorian (fromIntegral ye) 1 1) title
+
+  
+
+
+
+-- * Attoparsec parser combinators
 
 parseRows :: PT.Parser ByteString a -> PT.Parser ByteString [a]
-parseRows p = sepBy p endOfLine <* endOfInput
+parseRows p = many1 (p <* endOfLine)
 
--- a block of rows starting with an integer identifier
-stanza :: PT.Parser ByteString a -> PT.Parser ByteString (Integer, [a])
+-- a "stanza" is a block of rows starting starting with the movie ID and a colon. 
+stanza :: PT.Parser ByteString a -> PT.Parser ByteString (MovieId, [a])
 stanza p = do
-  i <- ident
+  i <- ident <* endOfLine
   pp <- many1 (p <* endOfLine)
-  return (i, pp)
+  return (MovieId (fromIntegral i), pp)
+
+
+
+-- * Attoparsec helpers
+
+date :: PT.Parser ByteString Day
+date = do
+  (yy:mm:dd:_) <- sepBy decimal dash
+  pure $ fromGregorian (fromIntegral yy) mm dd
 
 comma, dash :: Parser Char
 comma = char ','
 dash = char '-'
+
+decc :: PT.Parser ByteString Int
 decc = do
   d <- decimal
   _ <- comma
@@ -62,58 +168,9 @@ ident :: PT.Parser ByteString Integer
 ident = do
   i <- decimal
   _ <- char ':'
-  endOfLine
   pure i
 
-trainRow :: PT.Parser ByteString TrainingSet
-trainRow = do
-  uid <- decc
-  rate <- decc
-  d <- date
-  let r = Rating (UserId uid) d
-  return $ TrainSet r rate
-  
-testRow :: PT.Parser ByteString TestSet
-testRow = do
-  uid <- decc
-  d <- date
-  let r = Rating (UserId uid) d
-  return $ TestSet r
-
-moviesRow :: PT.Parser ByteString Movie
-moviesRow = do
-  mo <- decc
-  ye <- decc
-  title <- takeWhile (inClass "a-z")
-  return $ Movie (MovieId mo) (fromGregorian (fromIntegral ye) 1 1) title
-
-date :: PT.Parser ByteString Day
-date = do
-  (yy:mm:dd:_) <- sepBy decimal dash
-  pure $ fromGregorian (fromIntegral yy) mm dd
 
 
 
 
-data Rating = Rating {userId :: UserId,
-                      ratingDate :: Day} deriving (Eq, Show)
-
--- training set
-
-newtype UserId = UserId {unUserId :: Int} deriving (Eq, Show)
-
-data TrainingSet = TrainSet {trainRating :: Rating,
-                             rating :: Int } deriving (Eq, Show)
-
--- movies file
-
-newtype MovieId = MovieId {unMovieId :: Int} deriving (Eq, Show)
-
-data Movie = Movie { movieId :: MovieId,
-                     releaseYear :: Day,
-                     movieTitle :: ByteString } deriving (Eq, Show)
-
-
--- "qualifying" file (test set)
-
-newtype TestSet = TestSet { testRating :: Rating } deriving (Eq, Show)
