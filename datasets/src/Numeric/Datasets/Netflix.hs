7178080265b09bde999cb55eb31eebb5ef02ecf2
ocramz <zocca.marco@gmail.com>
2017-04-16 13:26:11 +0200

    parsers work

--- datasets/src/Numeric/Datasets/Netflix.hs
+++ datasets/src/Numeric/Datasets/Netflix.hs
@@ -15,10 +15,20 @@ For further information, see <http://netflixprize.com/>
 
 module Numeric.Datasets.Netflix where
 
+import Prelude hiding (takeWhile)
+
 import Numeric.Datasets
-import Data.Csv
+-- import Data.Csv
 import Data.FileEmbed
-import Data.ByteString
+import Data.ByteString hiding (takeWhile)
+import Data.Time (Day, fromGregorian)
+
+import qualified Data.Attoparsec.Internal.Types as PT (Parser) 
+import Data.Attoparsec.ByteString
+import Data.Attoparsec.ByteString.Char8 hiding (takeWhile, inClass)
+
+
+
 
 trainingSet :: [(FilePath, ByteString)]
 trainingSet = $(embedDir "datafiles/netflix/training/")
@@ -28,23 +38,82 @@ testSet = $(embedDir "datafiles/netflix/test/")
 
 
 
-data Rating = Rating {userId :: UserId, ratingDate :: Date} deriving (Eq, Show)
+-- Attoparsec
+
+parseRows :: PT.Parser ByteString a -> PT.Parser ByteString [a]
+parseRows p = sepBy p endOfLine <* endOfInput
+
+-- a block of rows starting with an integer identifier
+stanza :: PT.Parser ByteString a -> PT.Parser ByteString (Integer, [a])
+stanza p = do
+  i <- ident
+  pp <- many1 (p <* endOfLine)
+  return (i, pp)
+
+comma, dash :: Parser Char
+comma = char ','
+dash = char '-'
+decc = do
+  d <- decimal
+  _ <- comma
+  return d
+
+ident :: PT.Parser ByteString Integer
+ident = do
+  i <- decimal
+  _ <- char ':'
+  endOfLine
+  pure i
+
+trainRow :: PT.Parser ByteString TrainingSet
+trainRow = do
+  uid <- decc
+  rate <- decc
+  d <- date
+  let r = Rating (UserId uid) d
+  return $ TrainSet r rate
+  
+testRow :: PT.Parser ByteString TestSet
+testRow = do
+  uid <- decc
+  d <- date
+  let r = Rating (UserId uid) d
+  return $ TestSet r
+
+moviesRow :: PT.Parser ByteString Movie
+moviesRow = do
+  mo <- decc
+  ye <- decc
+  title <- takeWhile (inClass "a-z")
+  return $ Movie (MovieId mo) (fromGregorian (fromIntegral ye) 1 1) title
+
+date :: PT.Parser ByteString Day
+date = do
+  (yy:mm:dd:_) <- sepBy decimal dash
+  pure $ fromGregorian (fromIntegral yy) mm dd
+
+
+
+
+data Rating = Rating {userId :: UserId,
+                      ratingDate :: Day} deriving (Eq, Show)
 
 -- training set
 
 newtype UserId = UserId {unUserId :: Int} deriving (Eq, Show)
-data Date = Date Int Int Int deriving (Eq, Show)  -- FIXME use `time`
 
-data TrainingSet = TrainSet {trainRating :: Rating, rating :: Int } deriving (Eq, Show)
+data TrainingSet = TrainSet {trainRating :: Rating,
+                             rating :: Int } deriving (Eq, Show)
 
 -- movies file
 
 newtype MovieId = MovieId {unMovieId :: Int} deriving (Eq, Show)
-type Year = Int    -- FIXME use `time`
 
-data Movies = Movie { movieId :: MovieId, releaseYear :: Year, movieTitle :: String } deriving (Eq, Show)
+data Movie = Movie { movieId :: MovieId,
+                     releaseYear :: Day,
+                     movieTitle :: ByteString } deriving (Eq, Show)
 
 
 -- "qualifying" file (test set)
 
-data TestSet = TestSet { testRating :: Rating } deriving (Eq, Show)
+newtype TestSet = TestSet { testRating :: Rating } deriving (Eq, Show)
