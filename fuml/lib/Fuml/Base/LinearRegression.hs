67775b1534f7a40ec26ed3744ff594e3cba2d0f8
Tom Nielsen <tanielsen@gmail.com>
2017-05-02 22:39:33 +0100

    weighted OLS

--- fuml/lib/Fuml/Base/LinearRegression.hs
+++ fuml/lib/Fuml/Base/LinearRegression.hs
@@ -2,7 +2,7 @@ module Fuml.Base.LinearRegression where
 
 import Numeric.LinearAlgebra
 
--- | Ordniary least squares
+-- | Ordinary least squares
 ols :: [(Vector Double, Double)] -> Vector Double
 ols xys =
   let x = fromRows $ map fst xys
@@ -10,6 +10,16 @@ ols xys =
       betaMat = inv (tr' x <> x) <> tr' x <> y
   in betaMat ! 0
 
+-- | Weighted ordinary least squares
+wols :: Vector Double -> [(Vector Double, Double)] -> Vector Double
+wols wvs xys =
+  let w = diag wvs
+      x = fromRows $ map fst xys
+      y = col $ map snd xys
+      betaMat = inv (tr' x <> w <> x) <> tr' x <> w <> y
+  in betaMat ! 0
+
+-- | Ridge regression
 ridge :: Matrix Double -> [(Vector Double, Double)] -> Vector Double
 ridge gamma xys =
   let x = fromRows $ map fst xys
