2b3a1651c325d1195fae9fa227321c417216a3f3
John Hedges <john@drystone.co.uk>
2017-05-02 19:49:39 +0100

    Adds CPU usage by process and user

--- dashdo-examples/src/StatView.hs
+++ dashdo-examples/src/StatView.hs
@@ -11,11 +11,14 @@ import Control.Concurrent (forkIO, threadDelay)
 import Control.Concurrent.MVar
 import Lucid
 import Data.Text (Text, unpack, pack)
+import Data.Text.Encoding (decodeUtf8)
 import Lens.Micro.Platform
+import System.Posix.User (UserEntry, userName, userID, getAllUserEntries)
 
 import Graphics.Plotly (plotly, layout, title, Trace, name)
 import Graphics.Plotly.Lucid
 import Graphics.Plotly.GoG
+import Graphics.Plotly.Simple
 import Graphics.Plotly.Histogram (histogram)
 
 data SysStats = SysStats
@@ -31,24 +34,37 @@ main = do
   forkIO (statGrab stats)
   runDashdo (theDashdo stats)
 
-theDashdo mvStats = Dashdo initv (const (readMVar mvStats)) example
+theDashdo mvStats = Dashdo initv (const (getStats mvStats)) example
 
-example :: Example -> [SysStats] -> SHtml Example ()
-example _ stats = wrap plotlyCDN $ do
+example :: Example -> ([SysStats], [Process], [UserEntry]) -> SHtml Example ()
+example _ (stats, ps, us) = wrap plotlyCDN $ do
   let theData = zip [1..] stats
       mkLine f = line (aes & x .~ fst & y .~ (f . snd)) theData
       cpuLoad = mkLine statsLoad
       memUsage = mkLine (fromIntegral . statsMem)
       diskRead = mkLine (fromIntegral . fst . statsDiskIO) & name ?~ "Read"
       diskWrite = mkLine (fromIntegral . snd . statsDiskIO) & name ?~ "Write"
+      processes = hbarChart $ map (decodeUtf8 . procName &&& procCPUPercent) ps 
+      userCPU u = let uid = fromIntegral (userID u)
+        in sum . map procCPUPercent . filter ((== uid) . procUid) $ ps
+      users = hbarChart . filter ((> 0) . snd) $ map (pack . userName &&& userCPU) us
 
   h2_ "Dashdo Load Monitor"
   manualSubmit
   toHtml $ plotly "foo" [cpuLoad] & layout . title ?~ "CPU load"
   toHtml $ plotly "bar" [memUsage] & layout . title ?~ "Memory Usage"
   toHtml $ plotly "baz" [diskRead, diskWrite] & layout . title ?~ "Disk IO"
+  toHtml $ plotly "ps" [processes] & layout . title ?~ "CPU Usage by Process"
+  toHtml $ plotly "us" [users] & layout . title ?~ "CPU Usage by User"
 
 initv = Example
+
+getStats :: MVar [SysStats] -> IO ([SysStats], [Process], [UserEntry])
+getStats mvStats = do
+  ss <- readMVar mvStats
+  ps <- runStats snapshots
+  us <- getAllUserEntries
+  return (ss, ps, us)
 
 statGrab :: MVar [SysStats] -> IO ()
 statGrab mvStats = diskStats >>= forever
