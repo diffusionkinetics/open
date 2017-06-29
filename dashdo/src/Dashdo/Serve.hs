aa970eb9e3935c82be1e3c5c4f45ff1fdfa6baa0
nike <hismatovns@gmail.com>
2017-06-29 14:43:49 +0500

    removed DashdoLink and json import in Serve

--- dashdo/src/Dashdo/Serve.hs
+++ dashdo/src/Dashdo/Serve.hs
@@ -14,14 +14,8 @@ import qualified Data.UUID as UUID
 import Data.Text.Lazy (Text, fromStrict)
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy as BLS
-import Data.Aeson hiding (json)
 
 import Control.Monad (forM_)
-
-data DashdoLink = DashdoLink String T.Text
-
-instance ToJSON DashdoLink where
-    toJSON (DashdoLink did title) = object ["did" .= did, "title" .= title]
 
 dashdoHandler :: Dashdo a -> IO ([Param] -> ActionM ())
 dashdoHandler d = do
@@ -52,7 +46,6 @@ serve :: Text -> [(String, T.Text, [Param] -> ActionM ())] -> IO ()
 serve iniHtml handlers = do
   uuid <- getRandomUUID
   -- this is obviously incorrect (if the form fields change dynamically)
-  let titles = map (\(did, title, _) -> DashdoLink did title) handlers
   scotty 3000 $ do
     middleware logStdout
     get "/js/dashdo.js" $ do
