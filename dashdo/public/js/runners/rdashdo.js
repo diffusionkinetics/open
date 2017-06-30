d19deb7a45fdd0fb66f7b07f4d97cd66f8f7b2ef
nike <hismatovns@gmail.com>
2017-07-01 02:00:10 +0500

    rdashdo: load data from the first endpoint automatically

--- dashdo/public/js/runners/rdashdo.js
+++ dashdo/public/js/runners/rdashdo.js
@@ -1,1 +1,9 @@
-// alert("Hey, I'm using fancy RDashdo")
+(function($) {
+  $(document).ready(function() {
+    $('#dashdo-form').dashdo({
+      ajax: true,
+      containerElement: $('#dashdo-main'),
+      endpoints: $('.dashdo-link').map(function() { return this.href }).get(),
+    })
+  })
+})(jQuery)
