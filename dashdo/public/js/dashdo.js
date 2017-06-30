5ff0a7e8716d7d3ec7830d1c309af0421f126094
nike <hismatovns@gmail.com>
2017-06-30 21:40:14 +0500

    dashdo jQuery plugin (a milestone - work in progress)

--- dashdo/public/js/dashdo.js
+++ dashdo/public/js/dashdo.js
@@ -1,109 +1,78 @@
-$(function(){
-  var uuid = null;
-  var dashdos = {};
-  var submitTimer = null;
+// TODO: in RDashdo form 'href' -> 'action' (to make it submit!)
+// TODO: ajax rendering
+// TODO: switch between dashdos
+// TODO: remove STATIC PILICY! wai-middleware-static
+(function ( $ ) {
+ 
+    $.fn.dashdo = function(options) {
+      /*
+      options:
 
-  var uuidLoop = function() {
-    $.get("/uuid").done(function(data) {
-      if (uuid != data) {
-        uuid = data;
-        // reload current or go to first dashdo
-        var did = $('#dashdo-form').attr('href');
-        if (typeof did == 'undefined' || !(did in dashdos))
-          did = $('.dashdo-link').first().attr('href');
-        switchDashdo(did);
+       basic:
+        - ajax: true / false (default: false),
+        - uuidUrl (default: '/uuid')
+        - uuidInterval (default: 1000)
+
+       ui:
+        - event on which dashdo changes
+        - (ajax-only!) container (which to (re-)render)
+      */
+
+      var settings = $.extend({
+        // These are the defaults.
+        ajax: false,
+        uuidUrl: "/uuid",
+        uuidInterval: 1000,
+      }, options )
+
+      var requestHtmlFromServer = function(url, data, onSuccess) {
+        $.ajax({
+          type: "POST",
+          url: url,
+          data: data,
+          success: onSuccess,
+        })
       }
-    }).always(function() {
-      setTimeout(uuidLoop, 1000);
-    });
-  };
 
-  var switchDashdo = function(did) {
-    if (did in dashdos) {
-      $('#dashdo-main').replaceWith(dashdos[did].main);
-      $('#dashdo-sidebar').replaceWith(dashdos[did].sidebar);
-      $('#dashdo-form').attr('href', did);
-      $('#dashdo-title').text($('.dashdo-link[href="'+did+'"]').text());
-
-      // add click handler for the plotly select controls
-      $('.dashdo-plotly-select .js-plotly-plot').each(function() {
-        var graph = $(this).get(0);
-        var attr = $(this).siblings('.dashdo-plotly-select-attr').attr('value');
-        var input = $(this).siblings('input').first();
-        var restyle = function() {
-          var value = input.attr('value');
-          if (value === "") {
-            Plotly.restyle(graph, { 'marker.color': '#1F77B4)' });
-          } else {
-            var os = graph.data[0][attr].map(function(p) {
-              return p == value ? '#1F77B4' : '#A5C8E1';
-            });
-            Plotly.restyle(graph, {'marker.color' : [os]}, [0]);
+      var resubmit = function() {
+        $("input", this).prop('readonly', true);
+        $(this).submit()
+      }.bind(this)
+      
+      $('input,select', this).each(function(i,e) {
+        $(e).change(function() {
+          if (typeof(manual_submit) == "undefined" || !manual_submit) {
+            resubmit()
           }
-        };
-        restyle();
-        $(this).get(0).on('plotly_click', function(data) {
-          if (input.attr('value') == data.points[0][attr]) {
-            input.attr('value', "");
-          } else {
-            input.attr('value', data.points[0][attr]);
-          }
-          restyle();
-          input.change();
         });
-      });
+      }.bind(this));
 
-      // set up periodic submit
-      clearTimeout(submitTimer);
-      var timeout = Math.min(
-          $(dashdos[did].main).find('.dashdo-periodic-submit')
-          .map(function() { return $(this).attr('value'); }).get());
-      if (timeout > 0) {
-        submitTimer = setTimeout(function() {
-          if ($('#dashdo-form').attr('href') == did) {
-            loadDashdo(did, $('#dashdo-form').serialize());
-          }
-        }, timeout);
-      }
-      if (dashdos[did].needsReload) {
-        loadDashdo(did, $('#dashdo-form').serialize());
-      }
-    } else {
-      loadDashdo(did, "");
+      var submitByAjax = function() {
+        requestHtmlFromServer(
+          $(e.target).attr("action"),
+          $(e.target).serialize(),
+          function(data) {
+            // TODO: ajax rendering
+            console.log('data', data)
+            console.log('this', this)
+          }.bind(this)
+        )
+      }.bind(this)
+
+      this.filter("form").on("submit", function(e) {
+        if(!settings.ajax) {
+          $("input", this).prop('readonly', true);
+          return // and then it actualy submits
+        }
+
+        e.preventDefault()  // no 'native' submitting on ajax versions
+      })
+
+      // TODO: #2 - uuid chage
+      // TODO: #3 - periodic submit
+      // TODO: #4 - manual submit
+
+      return this;
     }
-  };
-
-  var loadDashdo = function(did, data) {
-    $('#spinner').addClass('fa-spin');
-    $.ajax({
-      type: "POST",
-      url: did,
-      data: data,
-      success: function(r) {
-        dashdos[did] = {};
-        dashdos[did].main = $('<div id="dashdo-main"></div>')
-          .append($.parseHTML(r, null, true));
-        dashdos[did].sidebar = $('<div id="dashdo-sidebar"></div>')
-          .append(dashdos[did].main.find('.dashdo-sidebar').remove());
-        $('#spinner').removeClass('fa-spin');
-        switchDashdo(did);
-      }
-    });
-  };
-
-  // add click handlers for sidebar links
-  $('#sidebar-wrapper').on('click', '.dashdo-link', function(e) {
-    e.preventDefault();
-    switchDashdo($(this).attr('href'));
-  });
-
-  // add change handler for input elements
-  $('#dashdo-form').on('change', 'input,select', function(e) {
-    if ($(':submit').length == 0) {
-      loadDashdo($('#dashdo-form').attr('href'), $('#dashdo-form').serialize());
-      e.preventDefault();
-    }
-  });
-
-  uuidLoop();
-});
+ 
+}( jQuery ));
