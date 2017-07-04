ecc138817ccc722898aeb1a439b17621619ec05c
kphloonafh <kphloonafh@gmail.com>
2017-07-04 15:01:04 +0000

    Added Supervisor values for svm regression and classification

---
+++ fuml-svm/TestSVM.hs
@@ -0,0 +1,69 @@
+module Main where
+
+import Numeric.Datasets.Iris 
+import Numeric.Datasets.OldFaithful
+import Fuml.Svm
+import Fuml.Core
+import Fuml.Supervised.Accuracy
+
+import Data.Function (on)
+import Data.Tuple
+import AI.SVM.Simple
+import Control.Arrow
+import Data.Random
+import qualified Data.Vector.Storable as V
+
+toVector = swap . fmap V.fromList
+
+toList :: Iris -> (Int, [Double])
+toList (Iris a b c d cl) = (toNum cl, [a,b,c,d])  where
+  toNum Virginica = 0
+  toNum Setosa = -1
+  toNum Versicolor = 1
+
+oflist :: OldFaithful -> (V.Vector Double, Double)
+oflist (OF a b c) = (V.fromList [fromIntegral a, b], c)
+
+directSvmSimple :: IO ()
+directSvmSimple = do
+  (test, train) <- fmap (splitAt 20) . sample . shuffle . fmap toList $ iris
+
+  let ((Result c gamma acc), cl) = chehLin train
+
+      count :: Eq a => [(a,a)] -> Double
+      count xs = on (/) (realToFrac . length) (filter (uncurry (==)) xs) xs
+
+  print "test accuracy"
+  print . count $ fmap (classify cl) <$> test
+  print "training accuracy"
+  print acc
+
+testClassifier :: IO ()
+testClassifier = do
+  (test, train) <- fmap (splitAt 50) . sample . shuffle . fmap (toVector . toList) $ iris
+
+
+  let oneclass = runSupervisor' (oneClass 0.01 Linear) $ fmap (const ()) <$> filter ((== 1) . snd) train
+  let simple = runSupervisor' rbfSimple train
+  let basic = runSupervisor' (svc (C 8) (Polynomial 3 3 3) Nothing) train
+
+  let go a | a == 1 = In | otherwise = Out
+  print "oneclass" 
+  print $ accuracy (fmap go <$> test) oneclass 
+
+  print "simple accuracy"
+  print $ accuracy test simple
+
+  print "basic accuracy"
+  print $ accuracy test basic
+  
+testRegression :: IO ()
+testRegression = do
+  (test, train) <- fmap (splitAt 20) . sample . shuffle . fmap oflist $ oldFaithful
+  
+  let regr = runSupervisor' (svr (Epsilon 0.1 0.1) (Sigmoid 3e3 3e3)) train
+
+  print $ rmse test regr
+
+{-main = testRegression-}
+main = testClassifier
--- fuml-svm/lib/Fuml/Svm.hs
+++ fuml-svm/lib/Fuml/Svm.hs
@@ -1,5 +1,67 @@
 module Fuml.Svm
-  where
+  ( ClassifierType(..)
+  , Kernel(..)
+  , OneClassResult(..)
+  , RegressorType(..)
+  , rbfSimple
+  , svc
+  , svr
+  , oneClass
+  ) where
 
 import AI.SVM.Simple
+import Data.List (maximumBy)
+import Data.Function (on)
+import Control.Monad.Identity (Identity)
+import Control.DeepSeq (NFData)
 import Fuml.Core
+import qualified Data.Vector.Storable as VS
+
+rbfSimple :: (Ord o, NFData o) => Supervisor Identity o (Double, SVMClassifier o) o
+rbfSimple = Supervisor $ \_ theData ->
+  let scaled = vScale f theData
+      f = factor theData
+   in return . predWith f . snd . chehLin $ scaled
+
+svc :: Ord o =>
+     ClassifierType 
+  -> Kernel 
+  -> Maybe [(o, Double)] -- Training weights
+  -> Supervisor Identity o (Double, SVMClassifier o) o
+svc cltype kernel mbweights = Supervisor $ \_ theData ->
+  let scaled = vScale f theData
+      f = factor theData
+
+      classifier = case mbweights of 
+        Just weights -> snd $ trainWtdClassifier cltype kernel weights scaled
+        Nothing -> snd $ trainClassifier cltype kernel scaled
+   in return (predWith f classifier)
+
+predWith f cl = Predict (f, cl) (classify cl . VS.map (*f))
+
+factor :: [(VS.Vector Double, o)] -> Double
+factor = recip . VS.maximum . fst . maximumBy (compare `on` VS.maximum . fst)
+
+vScale = map . scaleSwap where
+  scaleSwap :: Double -> (VS.Vector Double, o) -> (o, VS.Vector Double)
+  scaleSwap factor = (\(v,o) -> (o, VS.map (* factor) v))
+
+oneClass :: 
+     Double -- 'nu' parameter
+  -> Kernel
+  -> Supervisor Identity () (Double, SVMOneClass) OneClassResult
+oneClass nu kernel = Supervisor $ \_ theData ->
+  let f = factor theData
+      scaled = VS.map (*f) . fst <$> theData
+      oneclass = snd $ trainOneClass nu kernel scaled
+   in return $ Predict (f, oneclass) (inSet oneclass . VS.map (*f))
+
+svr :: 
+     RegressorType
+  -> Kernel
+  -> Supervisor Identity Double (Double, SVMRegressor) Double
+svr rtype kernel = Supervisor $ \_ theData ->
+  let f = factor theData
+      scaled = vScale f theData
+      regressor = snd $ trainRegressor rtype kernel scaled
+   in return $ Predict (f, regressor) (predictRegression regressor . VS.map (*f))
