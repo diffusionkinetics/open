4b102eeb901232f674590567dc52b7282225ec1d
Tom Nielsen <tanielsen@gmail.com>
2017-07-08 11:23:49 +0100

    rdash wrapper

--- youido/lib/Youido/Serve.hs
+++ youido/lib/Youido/Serve.hs
@@ -12,7 +12,11 @@ import Network.Wai.Middleware.HttpAuth
 import Lucid
 import Lucid.Bootstrap
 import Lucid.Bootstrap3
+import qualified Lucid.Rdash as RD
+
 import Data.Text (Text)
+import Data.Monoid
+
 
 
 import Database.PostgreSQL.Simple
@@ -58,6 +62,39 @@ stdWrapper hdrMore sidebar h = doctypehtml_ $ do
     cdnBootstrapJS
     script_ [src_ "/js/dashdo.js"] ""
     script_ [src_ "/js/runners/base.js"] ""
+
+rdashWrapper :: Text -> Html () -> Html () -> Html () -> Html ()
+rdashWrapper hdTxt hdrMore sidebar h = doctypehtml_ $ do
+  head_ $ do
+    meta_ [charset_ "utf-8"]
+    cdnCSS
+    cdnThemeCSS
+    hdrMore
+  body_ $ do
+    RD.mkIndexPage (RD.mkHead hdTxt) $ RD.mkBody $ do
+      let cw = RD.mkPageContent h
+
+      RD.mkPageWrapperOpen sidebar cw
+    cdnJqueryJS
+    cdnBootstrapJS
+    script_ [src_ "/js/dashdo.js"] ""
+    script_ [src_ "/js/runners/base.js"] ""
+
+rdashSidebar :: [(Text, Text)] -> Html ()
+rdashSidebar links = do
+  let mklink :: (Text,Text) -> Html ()
+      mklink (title, dest) =
+        a_ [href_ dest] $
+          toHtml title <> i_ [class_ "fa fa-tachometer menu-icon"] mempty
+      sidebar = map mklink links
+      sb = RD.mkSidebar sidebarMain (span_ "Dashboards") $ sidebar
+      sbfoot = ""
+  RD.mkSidebarWrapper sb sbfoot
+
+sidebarMain :: (Monad m) => HtmlT m ()
+sidebarMain  = a_ [href_ "#"] $ do
+  "Dashdo"
+  span_ [class_ "menu-icon glyphicon glyphicon-transfer"] (return ())
 
 mkSidebar :: [(Text, Text)] -> Html ()
 mkSidebar links = ul_ $ mapM_ f links where
