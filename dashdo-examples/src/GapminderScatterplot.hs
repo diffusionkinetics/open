a81c88ff1af358a2d66a09a53079d6dc6c6524ee
nike <hismatovns@gmail.com>
2017-07-12 11:44:25 +0500

    separate function for coutriesTable

--- dashdo-examples/src/GapminderScatterplot.hs
+++ dashdo-examples/src/GapminderScatterplot.hs
@@ -29,29 +29,32 @@ makeLenses ''GmParams
 
 gm0 = GmParams [] []
 
-gmRenderer :: [Gapminder] -> GmParams -> () ->  SHtml GmParams ()
+countriesTable :: [Gapminder] -> SHtml GmParams ()
+countriesTable gms =
+  table_ [class_ "table table-striped"] $ do
+    thead_ $ do
+      tr_ $ do
+        th_ "Country"
+        th_ "Year"
+        th_ "Population"
+        th_ "Gdp per capita"
+        th_ "Life expectancy"
+    tbody_ $ do
+      DF.for_ gms $ \(c) -> do
+        tr_ $ do
+          td_ $ toHtml (country          c)
+          td_ $ toHtml (show $ year      c)
+          td_ $ toHtml (show $ pop       c)
+          td_ $ toHtml (show $ gdpPercap c)
+          td_ $ toHtml (show $ lifeExp   c)
+
+gmRenderer :: [Gapminder] -> GmParams -> () -> SHtml GmParams ()
 gmRenderer gms gmParams () =
   wrap plotlyCDN $ do
     h1_ "Gapminder scatterplot example"
     row_ $ do
       mkCol [(MD,12)] $ do
-        table_ [class_ "table table-striped"] $ do
-          thead_ $ do
-            tr_ $ do
-              th_ "Country"
-              th_ "Year"
-              th_ "Population"
-              th_ "Gdp per capita"
-              th_ "Life expectancy"
-          
-          tbody_ $ do
-            DF.for_ gms $ \(c) -> do
-              tr_ $ do
-                td_ $ toHtml (country          c)
-                td_ $ toHtml (show $ year      c)
-                td_ $ toHtml (show $ pop       c)
-                td_ $ toHtml (show $ gdpPercap c)
-                td_ $ toHtml (show $ lifeExp   c)
+        countriesTable gms
 
 main = do
   gms <- getDataset gapminder
