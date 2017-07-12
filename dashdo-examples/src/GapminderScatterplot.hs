16aab526c8e84a0b6400333ae5ee0bd9febad546
nike <hismatovns@gmail.com>
2017-07-12 13:06:29 +0500

    took year 2007 only; RGB's for continents

--- dashdo-examples/src/GapminderScatterplot.hs
+++ dashdo-examples/src/GapminderScatterplot.hs
@@ -29,13 +29,25 @@ makeLenses ''GmParams
 
 gm0 = GmParams [] []
 
+data Continent = Africa | Americas | Asia | Europe | Oceania deriving (Eq, Show, Read)
+
+continentRGB :: Continent -> RGB Int
+continentRGB Africa   = RGB 255 165 0
+continentRGB Americas = RGB 255 0 0
+continentRGB Asia     = RGB 128 0 128
+continentRGB Europe   = RGB 0 255 0
+continentRGB Oceania  = RGB 0 0 255
+continentRGB _ = undefined
+
+-- TODO: 1 - doghunt 2- sum gdp for region
+-- TODO: plotlySelectMultiple
+
 countriesTable :: [Gapminder] -> SHtml GmParams ()
 countriesTable gms =
   table_ [class_ "table table-striped"] $ do
     thead_ $ do
       tr_ $ do
         th_ "Country"
-        th_ "Year"
         th_ "Population"
         th_ "Gdp per capita"
         th_ "Life expectancy"
@@ -43,19 +55,32 @@ countriesTable gms =
       DF.for_ gms $ \(c) -> do
         tr_ $ do
           td_ $ toHtml (country          c)
-          td_ $ toHtml (show $ year      c)
           td_ $ toHtml (show $ pop       c)
           td_ $ toHtml (show $ gdpPercap c)
           td_ $ toHtml (show $ lifeExp   c)
+
+countriesScatterPlot :: [Gapminder] -> SHtml GmParams ()
+countriesScatterPlot gms =
+  let
+    trace :: Trace
+    trace = points (aes & x .~ gdpPercap
+                        & y .~ lifeExp
+                        & color .~ Just (continentRGB . read . unpack . continent)) gms
+  in toHtml $ plotly "scatterplot" [trace]
 
 gmRenderer :: [Gapminder] -> GmParams -> () -> SHtml GmParams ()
 gmRenderer gms gmParams () =
   wrap plotlyCDN $ do
     h1_ "Gapminder scatterplot example"
     row_ $ do
+      mkCol [(MD,8)] $ do
+        countriesScatterPlot gms
+      mkCol [(MD,4)] $ do
+        "Doghunt chart goes here"
+    row_ $ do
       mkCol [(MD,12)] $ do
         countriesTable gms
 
 main = do
   gms <- getDataset gapminder
-  runDashdo $ Dashdo gm0 (return . const ()) (gmRenderer gms)
+  runDashdo $ Dashdo gm0 (return . const ()) (gmRenderer $ filter ((== 2007) . year) gms)
