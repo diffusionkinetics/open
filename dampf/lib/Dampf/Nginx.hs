bf57aaa82865be809217866fc49b6473aa3780f1
Alex McKenna <alexmckenna@outlook.com>
2017-07-13 16:36:52 +0100

    Refactor nginx
    The code in the Nginx modules has been refactored to use the HasFoo
    typeclasses defined in ConfigFile to specify the configuration needed.

--- dampf/lib/Dampf/Nginx.hs
+++ dampf/lib/Dampf/Nginx.hs
@@ -1,43 +1,51 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings  #-}
 
 module Dampf.Nginx where
 
-import Dampf.AppFile
-import Dampf.ConfigFile
-import System.Process
-import Control.Monad
-import System.Exit
-import System.Directory
-import System.FilePath
-import Data.Text (Text)
+import           Control.Monad
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Dampf.Nginx.Config
-import System.Posix.Files
+import           System.Directory
+import           System.FilePath
+import           System.Posix.Files
+import           System.Process
 
-deployDomains :: DampfConfig -> Dampfs -> IO ()
-deployDomains cfg (Dampfs dampfs) = do
-  forM_ [(nm,dspec) | Domain nm dspec <- dampfs] $ \(nm,dspec) -> do
-    --move static items
-    case static dspec of
-      Nothing -> return ()
-      Just sdir -> do
-        let dest = "/var/www" </> nm
-        ex <- doesDirectoryExist dest
-        when ex $ removeDirectoryRecursive dest
-        createDirectoryIfMissing True dest
-        system $ "cp -R "++(sdir</>".")++" "++dest
-        return ()
+import           Dampf.AppFile
+import           Dampf.ConfigFile
+import           Dampf.Nginx.Config
 
-    --create file
-    let fl = domainConfig cfg (T.pack nm) dspec
-    T.writeFile ("/etc/nginx/sites-available"</>nm) fl
 
-    removeIfExists ("/etc/nginx/sites-enabled"</>nm)
-    createSymbolicLink ("/etc/nginx/sites-available"</>nm) ("/etc/nginx/sites-enabled"</>nm)
-    system "service nginx reload"
-    return ()
+deployDomains :: (HasDampfConfig c) => c -> Dampfs -> IO ()
+deployDomains cfg (Dampfs dampfs) = forM_ ds $ \(d,dSpec) -> do
+    moveStaticItems (static dSpec) d
 
+    let fl = domainConfig cfg (T.pack d) dSpec
+    T.writeFile ("/etc/nginx/sites-available" </> d) fl
+
+    removeIfExists ("/etc/nginx/sites-enabled"</> d)
+    createSymbolicLink ("/etc/nginx/sites-available"</> d)
+        ("/etc/nginx/sites-enabled" </> d)
+
+    void $ system "service nginx reload"
+  where
+    ds = [(d, dSpec) | Domain d dSpec <- dampfs]
+
+
+moveStaticItems :: Maybe FilePath -> String -> IO ()
+moveStaticItems (Just src) s = do
+    exists <- doesDirectoryExist dest
+    when exists $ removeDirectoryRecursive dest
+
+    createDirectoryIfMissing True dest
+    void . system $ "cp -R " ++ (src </> ".") ++ " " ++ dest
+  where
+    dest = "/var/www" </> s
+
+moveStaticItems _        _ = return ()
+
+
+removeIfExists :: FilePath -> IO ()
 removeIfExists fp = do
-  ex <- doesFileExist fp
-  when ex $ removeFile fp
+    ex <- doesFileExist fp
+    when ex $ removeFile fp
+
