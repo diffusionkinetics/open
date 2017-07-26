4b9edca4b410f07bb6cba2cbf9c4dedab0f25a29
Alex McKenna <alexmckenna@outlook.com>
2017-07-26 15:35:08 +0100

    Make simple functions polymorphic
    Functions in simple are (where applicable) now polymorphic over any
    type which can be converted into JSON.

--- plotlyhs/src/Graphics/Plotly/Simple.hs
+++ plotlyhs/src/Graphics/Plotly/Simple.hs
@@ -1,47 +1,57 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 {-|
-
-Functions to build Traces from standard data. Generated traces can still be customized with lenses
-
+Functions to build Traces from standard data. Generated traces can still be
+customized with lenses.
 -}
-
 module Graphics.Plotly.Simple where
 
-import Graphics.Plotly.Base
-import Lens.Micro
-import Data.Text (Text)
 import Data.Aeson
+import Data.Text (Text)
+import Lens.Micro
+
+import Graphics.Plotly.Base
 
 
 -- |Generate a scatterplot from pairs
-scatterPlot :: [(Double,Double)] -> Trace
-scatterPlot xys = scatter & x ?~ map (toJSON .fst) xys
-                          & y ?~ map (toJSON .snd) xys
-                          & mode ?~ [Markers]
+scatterPlot :: (ToJSON a, ToJSON b) => [(a, b)] -> Trace
+scatterPlot xys = scatter
+    & x     ?~ fmap (toJSON . fst) xys
+    & y     ?~ fmap (toJSON . snd) xys
+    & mode  ?~ [Markers]
+
+
 -- |Generate a line plot from pairs
-linePlot :: [(Double,Double)] -> Trace
-linePlot xys    = scatter & x ?~ map (toJSON .fst) xys
-                          & y ?~ map (toJSON .snd) xys
-                          & mode ?~ [Lines]
+linePlot :: (ToJSON a, ToJSON b) => [(a, b)] -> Trace
+linePlot xys = scatter
+    & x     ?~ fmap (toJSON . fst) xys
+    & y     ?~ fmap (toJSON . snd) xys
+    & mode  ?~ [Lines]
+
 
 -- |Generate a horizontal bar chart from pairs of text and value.
 hbarChart :: [(Text, Double)] -> Trace
-hbarChart tvs = bars & y ?~ map (toJSON . fst) tvs
-                     & x ?~ map (toJSON . snd) tvs
-                     & orientation ?~ Horizontal
+hbarChart tvs = bars
+    & y             ?~ fmap (toJSON . fst) tvs
+    & x             ?~ fmap (toJSON . snd) tvs
+    & orientation   ?~ Horizontal
+
 
 -- |Generate a horizontal bar chart from pairs of text and value.
 vbarChart :: [(Text, Double)] -> Trace
-vbarChart tvs = bars & x ?~ map (toJSON . fst) tvs
-                     & y ?~ map (toJSON . snd) tvs
-                     & orientation ?~ Vertical
+vbarChart tvs = bars
+    & x             ?~ fmap (toJSON . fst) tvs
+    & y             ?~ fmap (toJSON . snd) tvs
+    & orientation   ?~ Vertical
+
 
 -- |Generate a fan plot with a given width in standard deviations and
 --  (x,(y,sd)) data
 fanPlot :: Double -> [(Double, (Double, Double))] -> Trace
-fanPlot sdCount tmnsds =
-  let xs = map fst tmnsds ++ reverse (map fst tmnsds)
-      ys = map ((\(m,sd) -> m+sdCount*sd) . snd) tmnsds
-           ++ reverse ( map ((\(m,sd) -> m-sdCount*sd) . snd) tmnsds)
-  in  scatter & x ?~ map toJSON xs & y ?~ map toJSON ys & fill ?~ ToZeroY
+fanPlot sdCount tmnsds = scatter
+    & x     ?~ fmap toJSON xs
+    & y     ?~ fmap toJSON ys
+    & fill  ?~ ToZeroY
+  where
+    xs = fmap fst tmnsds ++ reverse (fmap fst tmnsds)
+    ys = fmap ((\(m, sd) -> m + sdCount * sd) . snd) tmnsds
+            ++ reverse (fmap ((\(m, sd) -> m - sdCount * sd) . snd) tmnsds)
+
