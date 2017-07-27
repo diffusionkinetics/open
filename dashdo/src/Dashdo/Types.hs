5737c064124386e5a655db0269ec86880f57a841
nike <hismatovns@gmail.com>
2017-07-27 20:03:44 +0500

    SHtml transformer + functions made undefined in Types.hs

--- dashdo/src/Dashdo/Types.hs
+++ dashdo/src/Dashdo/Types.hs
@@ -12,21 +12,21 @@ import Data.Monoid ((<>))
 type FormField t = (Int, t -> Text -> t)
 type FormFields t = [FormField t]
 
-type SHtml t a = HtmlT (StateT (Int,t,FormFields t) IO) a
+type SHtml m t = HtmlT (StateT (Int, t, [FormField t]) m) t
 
-data RDashdo = forall t. RDashdo
+data RDashdo m = forall t. RDashdo
   { rdFid    :: String
   , rdTitle  :: Text
-  , rdDashdo :: Dashdo t }
+  , rdDashdo :: Dashdo m t }
 
-data Dashdo t = Dashdo
+data Dashdo m t = Dashdo
   { initial :: t
-  , render :: SHtml t () }
+  , render :: SHtml m t }
 
-runSHtml :: t -> SHtml t () -> IO (FormFields t, TL.Text)
+runSHtml :: Monad m => t -> SHtml m t -> m (FormFields t, TL.Text)
 runSHtml val shtml = do
   let stT = renderTextT shtml
-  (t, (_, _, ffs)) <- runStateT stT (0, val, [])
+  (t, (_, _, ffs)) <- runStateT stT (0, val, [])  -- TODO: val in initial state can not match with () in stT
   return (ffs, t)
 
 mkFieldName :: Int -> Text
@@ -41,23 +41,23 @@ fieldName = name_ . mkFieldName
 fieldNameMultiple :: Int -> Attribute
 fieldNameMultiple = name_ . mkFieldNameMultiple
 
-fresh :: SHtml a Int
-fresh = do
+fresh :: Monad m => SHtml m Int
+fresh = undefined {-- do
   (n, v, ffs) <- lift $ get
   lift $ put (n+1, v, ffs)
-  return n
+  return n --}
 
-freshAndValue :: SHtml a (a, Int)
-freshAndValue = do
+freshAndValue :: Monad m => SHtml m (a, Int)
+freshAndValue = undefined {-- do
   (n, v, ffs) <- lift $ get
   lift $ put (n+1, v, ffs)
-  return (v, n)
+  return (v, n) --}
 
-putFormField :: FormField t -> SHtml t ()
-putFormField ff = do
+putFormField :: Monad m => FormField t -> SHtml m ()
+putFormField ff = undefined {-- do
   (n, v, ffs) <- lift $ get
   lift $ put (n, v, ff:ffs)
-  return ()
+  return () --}
 
 lensSetter :: ASetter' s a -> (s -> a -> s)
 lensSetter l x y = x & l .~ y
