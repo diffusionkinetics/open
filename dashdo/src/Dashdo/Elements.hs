19f9b789042304e571c5f8dd1a3fc7b067c1955c
Tom Nielsen <tanielsen@gmail.com>
2017-08-15 21:33:06 +0100

    dashdo fields are text based

--- dashdo/src/Dashdo/Elements.hs
+++ dashdo/src/Dashdo/Elements.hs
@@ -56,7 +56,7 @@ textInput f = do
   (_,val,_) <- lift $ get
 
   putFormField (n, lensSetter f)
-  input_ [type_ "text", fieldName n, value_ (val ^. f)]
+  input_ [type_ "text", name_ n, value_ (val ^. f)]
 
 select :: Eq b => [(Text, b)] -> Lens' a b -> SHtml a ()
 select opts f = do
@@ -65,7 +65,7 @@ select opts f = do
                  Nothing -> s
                  Just x -> lensSetter f s x
   putFormField (n, ft)
-  select_ [fieldName n] $ do
+  select_ [name_ n] $ do
     forM_ opts $ \(optNm, optVal) ->
       if val ^. f == optVal
          then option_ [value_ optNm, selected_ ""] $ toHtml optNm
@@ -82,7 +82,7 @@ numInput mmin mmax mstep f = do
                  Just x -> lensSetter f s x
   putFormField (n, ft)
   input_ (amin  ++ amax ++ astep ++ [type_ "number",
-                                     fieldName n,
+                                     name_ n,
                                      value_ (pack . show $ val ^. f) ])
 
 manualSubmit :: SHtml a ()
@@ -106,10 +106,10 @@ checkbox text vTrue vFalse f = do
   putFormField (n, ft)
   div_ [class_ "checkbox"] $
     label_ $ do
-      input_ $ [type_ "checkbox", id_ fid, fieldName n, value_ "true"] ++ checked
+      input_ $ [type_ "checkbox", id_ fid, name_ n, value_ "true"] ++ checked
       toHtml text
   -- if checkbox doesn't supply a value we get this one instead
-  input_ [type_ "hidden", fieldName n, value_ "false"]
+  input_ [type_ "hidden", name_ n, value_ "false"]
 
 resetLink :: SHtml a ()
 resetLink = do
@@ -122,7 +122,7 @@ plotlySelect plot f = do
   div_ [class_ "dashdo-plotly-select"] $ do
     toHtml plot
     resetLink
-    input_ [type_ "hidden", fieldName n, value_ (val ^. f)]
+    input_ [type_ "hidden", name_ n, value_ (val ^. f)]
 
 plotlySelectMultiple :: Plotly -> Lens' a [Text] -> SHtml a ()
 plotlySelectMultiple plot f = do
@@ -131,6 +131,6 @@ plotlySelectMultiple plot f = do
   div_ [class_ "dashdo-plotly-select"] $ do
     toHtml plot
     resetLink
-    input_ [type_ "hidden", class_ "dashdo-plotly-multi-select-names", value_ $ mkFieldNameMultiple n]
+    input_ [type_ "hidden", class_ "dashdo-plotly-multi-select-names", value_ $ (n<>"[]") ]
     forM_ (val ^. f) $ \(v) ->
-      input_ [type_ "hidden", fieldNameMultiple n, value_ v]
+      input_ [type_ "hidden", name_ (n<>"[]"), value_ v]
