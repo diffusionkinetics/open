9cef458085a13df4e3710dea308f1fbf5f436bbb
Tom Nielsen <tanielsen@gmail.com>
2017-10-04 13:35:41 +0100

    pyping example

---
+++ dampf/example/pyping/Dockerfile
@@ -0,0 +1,7 @@
+FROM python:3.6.3-slim
+
+COPY serve.py /root
+
+EXPOSE 8080
+
+CMD ["/root/serve.py"]
---
+++ dampf/example/pyping/serve.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# From https://daanlenaerts.com/blog/2015/06/03/create-a-simple-http-server-with-python-3/
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# HTTPRequestHandler class
+class testHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
+  # GET
+  def do_GET(self):
+        # Send response status code
+        self.send_response(200)
+
+        # Send headers
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+
+        # Send message back to client
+        message = "ping"
+        # Write content as utf-8 data
+        self.wfile.write(bytes(message, "utf8"))
+        return
+
+def run():
+  print('starting server...')
+
+  # Server settings
+  # Choose port 8080, for port 80, which is normally used for a http server, you need root access
+  server_address = ('127.0.0.1', 8080)
+  httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
+  print('running server...')
+  httpd.serve_forever()
+
+
+run()
