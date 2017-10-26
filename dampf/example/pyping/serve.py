b86e06ea64b3ef4f829d0b026aa1e1b894706f03
Tom Nielsen <tanielsen@gmail.com>
2017-10-26 13:00:58 +0100

    pyping listen everywhere

--- dampf/example/pyping/serve.py
+++ dampf/example/pyping/serve.py
@@ -27,7 +27,7 @@ def run():
 
   # Server settings
   # Choose port 8080, for port 80, which is normally used for a http server, you need root access
-  server_address = ('127.0.0.1', 8080)
+  server_address = ('', 8080)
   httpd = HTTPServer(server_address, testHTTPServer_RequestHandler)
   print('running server...')
   httpd.serve_forever()
