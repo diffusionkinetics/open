852deb11bf0488d3adca378f14d2c09addaf0fb8
Tom Nielsen <tanielsen@gmail.com>
2017-12-12 21:26:26 +0000

    dashdo JavaScript files through CDN

--- dashdo/src/Dashdo/Files.hs
+++ dashdo/src/Dashdo/Files.hs
@@ -4,6 +4,14 @@ module Dashdo.Files where
 import Data.FileEmbed
 import qualified Data.ByteString.Lazy as BLS
 import qualified Data.Text as T
+import Lucid
+import Lucid.PreEscaped
+
+
+dashdoCDN, dashdoCDNbase, dashdoCDNrdash :: Monad m => HtmlT m ()
+dashdoCDN = scriptSrc "https://cdn.diffusionkinetics.com/dashdo/0.1/dashdo.js"
+dashdoCDNbase = scriptSrc "https://cdn.diffusionkinetics.com/dashdo/0.1/base.js"
+dashdoCDNrdash = scriptSrc "https://cdn.diffusionkinetics.com/dashdo/0.1/rdash.js"
 
 dashdoJS :: BLS.ByteString
 dashdoJS = BLS.fromStrict $(embedFile "public/js/dashdo.js")
