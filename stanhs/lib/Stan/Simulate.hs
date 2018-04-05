0e80bdf215354e1f1a738fe5b7cda0503ae981ed
Tom Nielsen <tanielsen@gmail.com>
2018-04-05 17:00:28 +0100

    more stan eval

--- stanhs/lib/Stan/Simulate.hs
+++ stanhs/lib/Stan/Simulate.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE Rank2Types #-}
+
 module Stan.Simulate where
 
 import Stan.Data
@@ -6,3 +8,13 @@ import Stan.AST
 eval :: StanEnv -> Expr -> StanValue
 eval _ (LitInt x) = VInt x
 eval _ (LitFloat x) = VDouble $ realToFrac x
+eval e (BinOp "+" x y) = numOp2 (+) (eval e x) (eval e y)
+eval e (BinOp "-" x y) = numOp2 (-) (eval e x) (eval e y)
+eval e (BinOp "*" x y) = numOp2 (*) (eval e x) (eval e y)
+--eval e (BinOp "/" x y) = numOp2 (/) (eval e x) (eval e y)
+
+numOp2 :: (forall a . Num a => a -> a -> a) -> StanValue -> StanValue -> StanValue
+numOp2 f (VDouble x) (VDouble y) = VDouble $ f x y
+
+fracOp :: (forall a . Fractional a => a -> a) -> StanValue -> StanValue
+fracOp f (VDouble x) = VDouble $ f x
